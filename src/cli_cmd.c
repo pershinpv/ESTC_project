96ac8a3e9ff134f4b90ecdaceade59e640f55d5b
pershinpv <pershinpv@gmail.com>
2021-12-27 06:34:03 +0000

    Beta version for issue 11

--- src/cli_cmd.c
+++ src/cli_cmd.c
@@ -1,7 +1,6 @@
 #include <string.h>
 #include "cli_cmd.h"
 #include "cli_usb.h"
-#include "nvmc.h"
 
 static bool cli_cmd_calc_param_values(char const command[], uint16_t params[], size_t param_position, size_t const params_qty)
 {
@@ -43,42 +42,58 @@ static bool cli_cmd_calc_param_values(char const command[], uint16_t params[], s
         return true;
 }
 
-static cli_cmd_result_t handler_cmd_help(char const *usb_msg, uint8_t usb_msg_len)
+static size_t cli_cmd_calc_word(char const command[], char word[], size_t param_position)
 {
-    const size_t param_num = 0;
-    const char cmd_type[] = "help";
-    uint16_t params[param_num];
+    uint8_t char_position = 0;
 
-    char *help_msg[CLI_COMMANDS_NUMBER + 1];
-    cli_cmd_result_t result = CLI_RES_NOT_FOUND;
+    if (command[param_position] == ' ')
+        do
+        {
+            ++param_position;
+        } while (command[param_position] == ' ');
+    else
+        return param_position = 0;
 
-    if (cli_cmd_calc_param_values(usb_msg, params, sizeof(cmd_type) - 1, param_num))
+    if((command[param_position] >='0' && command[param_position] <='9') || command[param_position] =='_')
+        return param_position = 0;
+
+    while ((command[param_position] >='A' && command[param_position] <='Z') || \
+           (command[param_position] >='a' && command[param_position] <='z') || \
+           (command[param_position] >='0' && command[param_position] <='9') || \
+           command[param_position] =='_')
     {
-        help_msg[0] = "Support commands:";
-        help_msg[1] = "1. 'help' to see this list";
-        help_msg[2] = "2. RGB <r> <g> <b>. r, g, b: (0...255)";
-        help_msg[3] = "3. HSV <h> <s> <v>. h: (0...360); s, v: (0...100)";
-        help_msg[4] = "4. No command yet";
-
-        for (size_t i = 0; i < CLI_COMMANDS_NUMBER + 1; ++i)
-            cli_usb_send_message(help_msg[i], strlen(help_msg[i]), true);
-
-        result = CLI_RES_OK;
+        if (char_position < CLI_COLOR_NAME_MAX_LEN - 1)
+        {
+            word[char_position] = command[param_position];
+        }
+        else
+        {
+            return param_position = 0;
+        }
+        ++char_position;
+        ++param_position;
     }
-    return result;
+
+    word[char_position++] = '\0';
+
+    while (char_position < SIZE_OF_COLOR_NAME_MAX_BYTES)
+    {
+        word[char_position] = 0xFF;
+        ++char_position;
+    }
+    return param_position;
 }
 
-static cli_cmd_result_t handler_cmd_rgb(char const *usb_msg, uint8_t usb_msg_len)
+static cli_cmd_result_t handler_cmd_rgb(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
 {
     const size_t param_num = 3;
-    const char cmd_type[] = "RGB";
     size_t cmd_msg_len = sizeof("Color set to ") + usb_msg_len - 1;
     rgb_t rgb;
     uint16_t params[param_num];
     char cmd_msg[cmd_msg_len];
     cli_cmd_result_t result = CLI_RES_NOT_FOUND;
 
-    if (cli_cmd_calc_param_values(usb_msg, params, sizeof(cmd_type) - 1, param_num))
+    if (cli_cmd_calc_param_values(usb_msg, params, strlen(cmd_name), param_num))
     {
         if (params[0] <= RGB_MAX_VAL && params[1] <= RGB_MAX_VAL && params[2] <= RGB_MAX_VAL)
         {
@@ -87,11 +102,9 @@ static cli_cmd_result_t handler_cmd_rgb(char const *usb_msg, uint8_t usb_msg_len
             rgb.b = (uint8_t)params[2];
             rgb.crc = color_crc_calc_rgb(&rgb);
             color_value_set_rgb(&rgb);
-            cmd_msg_len = sprintf(cmd_msg, "Color set to %s %u %u %u", cmd_type, params[0], params[1], params[2]);
+            cmd_msg_len = sprintf(cmd_msg, "Color set to %s %u %u %u", cmd_name, params[0], params[1], params[2]);
             cli_usb_send_message(cmd_msg, cmd_msg_len, true);
             nvmc_write_rgb_actual_values(&rgb);
-            nvmc_read_rgb_actual_values_for_log();
-
             result = CLI_RES_OK;
         }
         else
@@ -102,10 +115,9 @@ static cli_cmd_result_t handler_cmd_rgb(char const *usb_msg, uint8_t usb_msg_len
     return result;
 }
 
-static cli_cmd_result_t handler_cmd_hsv(char const *usb_msg, uint8_t usb_msg_len)
+static cli_cmd_result_t handler_cmd_hsv(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
 {
     const size_t param_num = 3;
-    const char cmd_type[] = "HSV";
     size_t cmd_msg_len = sizeof("Color set to ") + usb_msg_len - 1;
     rgb_t rgb;
     hsv_t hsv;
@@ -113,7 +125,7 @@ static cli_cmd_result_t handler_cmd_hsv(char const *usb_msg, uint8_t usb_msg_len
     char cmd_msg[cmd_msg_len];
     cli_cmd_result_t result = CLI_RES_NOT_FOUND;
 
-    if (cli_cmd_calc_param_values(usb_msg, params, sizeof(cmd_type) - 1, param_num))
+    if (cli_cmd_calc_param_values(usb_msg, params, strlen(cmd_name), param_num))
     {
         if (params[0] <= HSV_MAX_H && params[1] <= HSV_MAX_S && params[2] <= HSV_MAX_V)
         {
@@ -122,11 +134,9 @@ static cli_cmd_result_t handler_cmd_hsv(char const *usb_msg, uint8_t usb_msg_len
             hsv.v = (uint8_t)params[2];
             color_hsv_to_rgb(&hsv, &rgb);
             color_value_set_rgb(&rgb);
-            cmd_msg_len = sprintf(cmd_msg, "Color set to %s %u %u %u", cmd_type, params[0], params[1], params[2]);
+            cmd_msg_len = sprintf(cmd_msg, "Color set to %s %u %u %u", cmd_name, params[0], params[1], params[2]);
             cli_usb_send_message(cmd_msg, cmd_msg_len, true);
             nvmc_write_rgb_actual_values(&rgb);
-            nvmc_read_rgb_actual_values_for_log();
-
             result = CLI_RES_OK;
         }
         else
@@ -137,17 +147,172 @@ static cli_cmd_result_t handler_cmd_hsv(char const *usb_msg, uint8_t usb_msg_len
     return result;
 }
 
-static cli_cmd_result_t handler_cmd_save_rgb(char const *usb_msg, uint8_t usb_msg_len)
+static cli_cmd_result_t handler_cmd_add_rgb_color(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
 {
-    return CLI_RES_NOT_FOUND;
+    rgb_t rgb;
+    const size_t param_num = 3;
+    uint16_t params[param_num];
+    char color_name[CLI_COLOR_NAME_MAX_LEN];
+    cli_cmd_result_t result = CLI_RES_NOT_FOUND;
+
+    uint8_t param_pos = cli_cmd_calc_word(usb_msg, color_name, strlen(cmd_name));
+
+    if (param_pos > 0)
+    {
+        if (cli_cmd_calc_param_values(usb_msg, params, param_pos, param_num))
+        {
+            if (params[0] <= RGB_MAX_VAL && params[1] <= RGB_MAX_VAL && params[2] <= RGB_MAX_VAL)
+            {
+                rgb.r = (uint8_t)params[0];
+                rgb.g = (uint8_t)params[1];
+                rgb.b = (uint8_t)params[2];
+                rgb.crc = color_crc_calc_rgb(&rgb);
+
+                if(nvmc_add_rgb_color(&rgb, color_name))
+                {
+                    size_t cmd_msg_len = sizeof("Add color RGB ") + usb_msg_len - 1;
+                    char cmd_msg[cmd_msg_len];
+                    color_value_set_rgb(&rgb);
+                    cmd_msg_len = sprintf(cmd_msg, "Add color '%s' RGB %u %u %u", color_name, params[0], params[1], params[2]);
+                    cli_usb_send_message(cmd_msg, cmd_msg_len, true);
+                    nvmc_write_rgb_actual_values(&rgb);
+                }
+                else
+                {
+                    char msg[] = "Duplicated color name. Current color doesn't added.";
+                    cli_usb_send_message(msg, sizeof(msg), true);
+                }
+
+                result = CLI_RES_OK;
+            }
+            else
+            {
+                result = CLI_RES_NOT_FOUND;
+            }
+        }
+    }
+    return result;
+}
+
+static cli_cmd_result_t handler_cmd_del_color(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
+{
+    char color_name[CLI_COLOR_NAME_MAX_LEN];
+    uint8_t cmd_color_pos = cli_cmd_calc_word(usb_msg, color_name, strlen(cmd_name));
+
+    if (cmd_color_pos == 0)
+        return CLI_RES_NOT_FOUND;
+
+    while (usb_msg[cmd_color_pos] != '\0')
+    {
+        if (usb_msg[cmd_color_pos] != ' ')
+            return CLI_RES_NOT_FOUND;
+        ++cmd_color_pos;
+    }
+
+    if (nvmc_del_rgb_color(color_name))
+    {
+        size_t cmd_msg_len = sizeof("Color '' deleted") + usb_msg_len - 1;
+        char cmd_msg[cmd_msg_len];
+        cmd_msg_len = sprintf(cmd_msg, "Color '%s' deleted", color_name);
+        cli_usb_send_message(cmd_msg, cmd_msg_len, true);
+    }
+    else
+    {
+        char msg[] = "Color not found.";
+        cli_usb_send_message(msg, sizeof(msg), true);
+    }
+
+    return CLI_RES_OK;
+}
+
+static cli_cmd_result_t handler_cmd_apply_color(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
+{
+    rgb_t rgb;
+    char color_name[CLI_COLOR_NAME_MAX_LEN];
+    uint32_t color_addr = 0;
+    uint8_t cmd_color_pos = cli_cmd_calc_word(usb_msg, color_name, strlen(cmd_name));
+
+    if (cmd_color_pos == 0)
+        return CLI_RES_NOT_FOUND;
+
+    while (usb_msg[cmd_color_pos] != '\0')
+    {
+        if (usb_msg[cmd_color_pos] != ' ')
+            return CLI_RES_NOT_FOUND;
+        ++cmd_color_pos;
+    }
+
+    color_addr = nvmc_find_rgb_color(color_name);
+
+    if (color_addr > 0)
+    {
+        rgb.r = *(uint8_t *)(color_addr + 0);
+        rgb.g = *(uint8_t *)(color_addr + 1);
+        rgb.b = *(uint8_t *)(color_addr + 2);
+        rgb.crc = *(uint8_t *)(color_addr + 3);
+
+        if(color_rgb_values_validate(&rgb))
+        {
+            size_t cmd_msg_len = sizeof("Color '' applyed") + usb_msg_len - 1;
+            char cmd_msg[cmd_msg_len];
+
+            color_value_set_rgb(&rgb);
+            nvmc_write_rgb_actual_values(&rgb);
+            cmd_msg_len = sprintf(cmd_msg, "Color '%s' applyed", color_name);
+            cli_usb_send_message(cmd_msg, cmd_msg_len, true);
+        }
+        else
+        {
+            size_t cmd_msg_len = sizeof("Color '' CRC check faild. Color doesn't set.") + usb_msg_len - 1;
+            char cmd_msg[cmd_msg_len];
+
+            cmd_msg_len = sprintf(cmd_msg, "Color '%s' CRC check faild. Color doesn't set.", color_name);
+            cli_usb_send_message(cmd_msg, cmd_msg_len, true);
+        }
+    }
+    else
+    {
+        char msg[] = "Color not found.";
+        cli_usb_send_message(msg, sizeof(msg), true);
+    }
+
+    return CLI_RES_OK;
+}
+
+static cli_cmd_result_t handler_cmd_help(char const *cmd_name, char const *usb_msg, uint8_t usb_msg_len)
+{
+    const size_t param_num = 0;
+    uint16_t params[param_num];
+
+    char *help_msg[CLI_COMMANDS_NUMBER + 1];
+    cli_cmd_result_t result = CLI_RES_NOT_FOUND;
+
+    if (cli_cmd_calc_param_values(usb_msg, params, strlen(cmd_name), param_num))
+    {
+        help_msg[0] = "\nSupport commands:";
+        help_msg[1] = "1. 'help' to see this list";
+        help_msg[2] = "2. rgb <r> <g> <b>. r, g, b: (0...255)";
+        help_msg[3] = "3. hsv <h> <s> <v>. h: (0...360); s, v: (0...100)";
+        help_msg[4] = "4. add_rgb_color <color_name> <r> <g> <b>. r, g, b: (0...255); color_name MAX length: 23";
+        help_msg[5] = "5. del_color <color_name>";
+        help_msg[6] = "6. apply_color <color_name>";
+
+        for (size_t i = 0; i <= CLI_COMMANDS_NUMBER; ++i)
+            cli_usb_send_message(help_msg[i], strlen(help_msg[i]) + 1, true);
+
+        result = CLI_RES_OK;
+    }
+    return result;
 }
 
 static cli_cmd_cdescriptor_t cli_commands[] =
 {
     {"help", handler_cmd_help},
-    {"RGB", handler_cmd_rgb},
-    {"HSV", handler_cmd_hsv},
-    {"save_rgb_color", handler_cmd_save_rgb},
+    {"rgb", handler_cmd_rgb},
+    {"hsv", handler_cmd_hsv},
+    {"add_rgb_color", handler_cmd_add_rgb_color},
+    {"del_color", handler_cmd_del_color},
+    {"apply_color", handler_cmd_apply_color},
 };
 
 void cli_cmd_handler(char const *usb_msg, uint8_t usb_msg_len)
@@ -157,9 +322,10 @@ void cli_cmd_handler(char const *usb_msg, uint8_t usb_msg_len)
     for(size_t i = 0; i < ARRAY_SIZE(cli_commands); ++i)
     {
         const cli_cmd_cdescriptor_t *cli_cmd = &cli_commands[i];
-        if(strncmp(usb_msg, cli_cmd->cmd_name, sizeof(cli_cmd->cmd_name) - 1) == 0)
+
+        if(strncmp(usb_msg, cli_cmd->cmd_name, strlen(cli_cmd->cmd_name)) == 0)
         {
-            ret = cli_cmd->cmd_handler(usb_msg, usb_msg_len);
+            ret = cli_cmd->cmd_handler(cli_cmd->cmd_name, usb_msg, usb_msg_len);
         }
     }
 
@@ -167,5 +333,9 @@ void cli_cmd_handler(char const *usb_msg, uint8_t usb_msg_len)
     {
         char error_msg[] = "Unknown command. For help type 'help'.";
         cli_usb_send_message(error_msg, sizeof(error_msg), true);
+    }
+    else
+    {
+        nvmc_read_rgb_actual_values_for_log();
     }
 }
--- src/nvmc.c
+++ src/nvmc.c
@@ -2,6 +2,7 @@
 #include "log.h"
 #include "nrfx_nvmc.h"
 #include "nvmc.h"
+#include "cli_cmd.h"
 
 //find_first_free_pos_addr_between return 0 if free position not found
 static uint32_t find_first_free_pos_addr_between(uint32_t start_addr, uint32_t stop_addr )
@@ -30,7 +31,7 @@ static bool check_for_new_rgb_vals(rgb_t const *const rgb_vals, uint8_t const *c
     return !(rgb_vals->r == *address && rgb_vals->g == *(address + 1) && rgb_vals->b == *(address + 2) && rgb_vals->crc == *(address + 3));
 }
 
-static void nvmc_copy_good_vals_to_new_page(uint32_t const old_page_addr, uint32_t const new_page_addr, uint32_t const last_value)
+static uint32_t nvmc_copy_good_vals_to_new_page(uint32_t const old_page_addr, uint32_t const new_page_addr, uint32_t const last_value)
 {
     uint32_t old_color_name_addr = old_page_addr + SHIFT_ADDR_COLOR_NAMES;
     uint32_t old_color_vals_addr = old_page_addr + SHIFT_ADDR_COLOR_NAMES_VALS;
@@ -40,11 +41,13 @@ static void nvmc_copy_good_vals_to_new_page(uint32_t const old_page_addr, uint32
 
     while(*(uint32_t*)old_color_vals_addr != 0xFFFFFF && colors_counter < SIZE_OF_COLOR_NAME_BUFFER)
     {
-        if (*(uint32_t*)(old_color_name_addr + 1) >> 24 != DELETE_ATTRIBUTE)
+        if (*(uint8_t*)(old_color_name_addr + SIZE_OF_COLOR_NAME_MAX_BYTES - 1) != DELETE_ATTRIBUTE)
         {
-            nrfx_nvmc_word_write(new_color_name_addr, *(uint32_t*)old_color_name_addr);
-            nrfx_nvmc_word_write(new_color_name_addr + 1, *(uint32_t*)(old_color_name_addr + 4));
-            new_color_name_addr += SIZE_OF_COLOR_NAME_MAX_BYTES;
+            for (size_t cur_byte = 0; cur_byte < SIZE_OF_COLOR_NAME_MAX_BYTES - 1; cur_byte += sizeof(uint32_t))
+            {
+                nrfx_nvmc_word_write(new_color_name_addr, *(uint32_t*)(old_color_name_addr + cur_byte));
+                new_color_name_addr += sizeof(uint32_t);
+            }
 
             nrfx_nvmc_word_write(new_color_vals_addr, *(uint32_t*)old_color_vals_addr);
             new_color_vals_addr += SIZE_OF_COLOR_RGB_VALUE_BYTES;
@@ -55,6 +58,7 @@ static void nvmc_copy_good_vals_to_new_page(uint32_t const old_page_addr, uint32
         ++colors_counter;
     }
     nrfx_nvmc_word_write(new_page_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR, last_value);
+    return new_color_vals_addr;
 }
 
 /**
@@ -93,6 +97,116 @@ void nvmc_write_rgb_actual_values(rgb_t const *const rgb_vals)
     }
 }
 
+// is_color_name_exist return 0 if color name is new and return color address if the color name exist
+static uint32_t is_color_name_exist(uint32_t const page_start_addr, char const color_name[])
+{
+    for (size_t color_addr = page_start_addr + SHIFT_ADDR_COLOR_NAMES; color_addr < page_start_addr + SHIFT_ADDR_COLOR_NAMES_VALS;
+         color_addr += SIZE_OF_COLOR_NAME_MAX_BYTES)
+    {
+        bool is_next_color_name = false;
+        size_t current_char = 0;
+
+        if (*(uint8_t *)(color_addr + SIZE_OF_COLOR_NAME_MAX_BYTES - 1) == DELETE_ATTRIBUTE)
+            is_next_color_name = true;
+
+        while (!is_next_color_name && current_char < SIZE_OF_COLOR_NAME_MAX_BYTES - 1)
+        {
+            if (color_name[current_char] != *(uint8_t *)(color_addr + current_char))
+                is_next_color_name = true;
+            ++current_char;
+        }
+        if (!is_next_color_name && current_char == SIZE_OF_COLOR_NAME_MAX_BYTES - 1)
+            return color_addr;
+    }
+    return 0;
+}
+
+static void nvmc_write_color_name(uint32_t address, char const color_name[])
+{
+    uint32_t color_word = 0;
+
+    for (size_t cur_char = 0; cur_char < SIZE_OF_COLOR_NAME_MAX_BYTES; cur_char += sizeof(uint32_t))
+    {
+        color_word = (color_name[cur_char + 0]) + (color_name[cur_char + 1] << 8) \
+                   + (color_name[cur_char + 2] << 16) + (color_name[cur_char + 3] << 24);
+
+        nrfx_nvmc_word_write(address + cur_char, color_word);
+    }
+}
+
+bool nvmc_add_rgb_color(rgb_t const *const rgb_vals, char const color_name[])
+{
+    uint32_t rgb_word = rgb_vals->r + (rgb_vals->g  << 8) + (rgb_vals->b << 16) + (rgb_vals->crc  << 24);
+    uint32_t active_page_start_addr = 0;
+    uint32_t current_color_vals_address = 0;
+    uint32_t current_color_name_address = 0;
+
+    active_page_start_addr = get_active_page_start_addr(APP_DATA_START_ADDR);
+    current_color_vals_address = find_first_free_pos_addr_between(active_page_start_addr + SHIFT_ADDR_COLOR_NAMES_VALS, \
+                                                                  active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR);
+
+    if (is_color_name_exist(active_page_start_addr, color_name) > 0)
+        return false;
+
+    /*NRF_LOG_INFO("page_addr %d color_name %d %d %d %d %d", active_page_start_addr, color_name[0], color_name[1], color_name[2], \
+                                                                                   color_name[3], color_name[4]);
+*/
+    if (current_color_vals_address == 0)
+    {
+        uint32_t new_page_start_address = (active_page_start_addr == APP_DATA_START_ADDR) ? \
+                                          (active_page_start_addr + PAGE_SIZE_BYTES) : active_page_start_addr;
+
+        current_color_vals_address = nvmc_copy_good_vals_to_new_page(active_page_start_addr, new_page_start_address, rgb_word);
+        current_color_name_address = active_page_start_addr + \
+        ((current_color_vals_address - active_page_start_addr - SHIFT_ADDR_COLOR_NAMES_VALS) / 4) * SIZE_OF_COLOR_NAME_MAX_BYTES;
+
+        nvmc_write_color_name(current_color_name_address, color_name);
+        nrfx_nvmc_word_write(current_color_vals_address, rgb_word);
+        nrfx_nvmc_page_erase(active_page_start_addr);
+    }
+    else
+    {
+        current_color_name_address = active_page_start_addr + \
+        ((current_color_vals_address - active_page_start_addr - SHIFT_ADDR_COLOR_NAMES_VALS) / 4) * SIZE_OF_COLOR_NAME_MAX_BYTES;
+
+        nvmc_write_color_name(current_color_name_address, color_name);
+        nrfx_nvmc_word_write(current_color_vals_address, rgb_word);
+    }
+
+    return true;
+}
+
+bool nvmc_del_rgb_color(char const color_name[])
+{
+    uint32_t color_addr = 0;
+
+    color_addr = is_color_name_exist(get_active_page_start_addr(APP_DATA_START_ADDR), color_name);
+
+    if (color_addr == 0)
+    {
+        return false;
+    }
+    else
+    {
+        nrfx_nvmc_byte_write(color_addr + SIZE_OF_COLOR_NAME_MAX_BYTES - 1, DELETE_ATTRIBUTE);
+        return true;
+    }
+}
+
+//return address of RGB values for color_name or 0 if color_name not found
+uint32_t nvmc_find_rgb_color(char const color_name[])
+{
+    uint32_t color_addr = is_color_name_exist(get_active_page_start_addr(APP_DATA_START_ADDR), color_name);
+    if (color_addr > 0)
+    {
+        return (color_addr - get_active_page_start_addr(APP_DATA_START_ADDR) - SHIFT_ADDR_COLOR_NAMES) / \
+                SIZE_OF_COLOR_NAME_MAX_BYTES * SIZE_OF_COLOR_RGB_VALUE_BYTES + SHIFT_ADDR_COLOR_NAMES_VALS + \
+                get_active_page_start_addr(APP_DATA_START_ADDR);
+    }
+    else
+        return 0;
+}
+
 void nvmc_read_rgb_actual_values(rgb_t *const rgb_vals)
 {
     uint32_t active_page_start_addr;
@@ -116,24 +230,55 @@ void nvmc_read_rgb_actual_values(rgb_t *const rgb_vals)
 void nvmc_read_rgb_actual_values_for_log()
 {
     uint32_t active_page_start_addr;
-    uint32_t current_read_address;
+    uint32_t current_last_val_address;
+    uint32_t current_color_vals_address;
+    size_t number_of_saved_colors = 0;
 
     active_page_start_addr = get_active_page_start_addr(APP_DATA_START_ADDR);
-    current_read_address = find_first_free_pos_addr_between(active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR, \
+
+    current_color_vals_address = find_first_free_pos_addr_between(active_page_start_addr + SHIFT_ADDR_COLOR_NAMES_VALS, \
+                                                                  active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR);
+
+    if (current_color_vals_address == 0)      //0 if the page is full
+        current_color_vals_address = active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR - sizeof(uint32_t);
+    else
+        current_color_vals_address -= sizeof(uint32_t);
+
+    number_of_saved_colors = (current_color_vals_address - active_page_start_addr - SHIFT_ADDR_COLOR_NAMES_VALS + 4) / 4;
+
+    for (size_t curr_color_num = 0; curr_color_num < number_of_saved_colors; ++curr_color_num)
+    {
+        uint32_t vals_addr = active_page_start_addr + SHIFT_ADDR_COLOR_NAMES_VALS + curr_color_num * SIZE_OF_COLOR_RGB_VALUE_BYTES;
+        uint32_t name_adrr = active_page_start_addr + SHIFT_ADDR_COLOR_NAMES + curr_color_num * SIZE_OF_COLOR_NAME_MAX_BYTES;
+        char color_name[SIZE_OF_COLOR_NAME_MAX_BYTES];
+
+        for(size_t char_counter = 0; char_counter < SIZE_OF_COLOR_NAME_MAX_BYTES; ++char_counter)
+            color_name[char_counter] = *(uint8_t*)(name_adrr + char_counter);
+
+        NRF_LOG_INFO("Name address: 0x%X Name: %c%c%c%c Delete attribute: %d", \
+                      name_adrr, color_name[0] % 255, color_name[1] % 255, color_name[2] % 255, color_name[3] % 255, \
+                                 color_name[SIZE_OF_COLOR_NAME_MAX_BYTES - 1] % 255);
+
+        NRF_LOG_INFO("Vals address: 0x%X r=%d g=%d b=%d crc=%d",
+                     vals_addr, *(uint8_t *)vals_addr, *(uint8_t *)(vals_addr + 1), *(uint8_t *)(vals_addr + 2), *(uint8_t *)(vals_addr + 3));
+    }
+
+    NRF_LOG_INFO("-----Reading %d colors values (%d bytes) finished-----", number_of_saved_colors, number_of_saved_colors * 4);
+
+    current_last_val_address = find_first_free_pos_addr_between(active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR, \
                                                             active_page_start_addr + PAGE_SIZE_BYTES);
 
-    if (current_read_address == 0)      //0 if the page is full
-        current_read_address = active_page_start_addr + PAGE_SIZE_BYTES - sizeof(uint32_t);
+    if (current_last_val_address == 0)      //0 if the page is full
+        current_last_val_address = active_page_start_addr + PAGE_SIZE_BYTES - sizeof(uint32_t);
     else
-        current_read_address -= sizeof(uint32_t);
+        current_last_val_address -= sizeof(uint32_t);
 
-    for (uint32_t addr = active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR; addr <= current_read_address; addr += sizeof(uint32_t))
+    for (uint32_t addr = active_page_start_addr + SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR; addr <= current_last_val_address; addr += sizeof(uint32_t))
     {
         NRF_LOG_INFO("At address 0x%X r=%d g=%d b=%d crc=%d", addr, \
         *(uint8_t *)addr, *(uint8_t *) (addr + 1), *(uint8_t *) (addr + 2), *(uint8_t *) (addr + 3));
-        //nrf_delay_ms(10);
     }
     NRF_LOG_INFO("-----Reading %d bytes (%d records) finished-----", \
-     current_read_address - active_page_start_addr - SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR + 4, \
-    (current_read_address - active_page_start_addr - SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR + 4) / 4);
+     current_last_val_address - active_page_start_addr - SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR + 4, \
+    (current_last_val_address - active_page_start_addr - SHIFT_ADDR_LAST_RGB_VAL_LST_ADDR + 4) / 4);
 }
